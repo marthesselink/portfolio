5ca55c58ae75f07fd2a94276f1d97e77f32b847b
Mart Hesselink <marthesselink@hotmail.com>
2021-02-18 16:46:27 +0100

    :lipstick: Add Github and global styling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <Meta/>
   <Header/>
-
+  <Github/>
   <RecentProjects/>
 </template>
 
 <script>
   import Meta from './components/Meta.vue'
+  import Github from './components/Github.vue'
   import Header from './components/Header.vue'
   import RecentProjects from './components/RecentProjects.vue'
 
@@ -14,6 +15,7 @@
     name: 'App',
     components: {
       Meta,
+      Github,
       Header,
       RecentProjects
     }
@@ -23,21 +25,35 @@
 <style lang="scss">
   html {
     scroll-behavior: smooth;
+    max-width: 1140px;
+    margin: 0 auto;
+    width: 100%;
+    padding: 40px 40px 60px 40px;
   }
 
-  h1 {
+  h1,
+  h2,
+  h3 {
+    font-family: Avenir Next;
     font-weight: bold;
   }
 
+  p {
+    font-family: Avenir Next;
+  }
+
+  h1 {
+    font-weight: 700;
+    font-size: 50px;
+  }
+
   h2 {
-    margin-bottom: 1rem;
+    font-weight: 700;
+    font-size: 45px;
   }
 
   h3 {
-    font-size: 1.5rem;
-
-    @media(min-width: 992px) {
-      font-size: 2rem;
-    }
+    font-weight: 700;
+    font-size: 25px;
   }
 </style>
