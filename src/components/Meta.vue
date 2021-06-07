6e63dfc8aeb2db3d24feae31ace527d272c756c3
Mart Hesselink <marthesselink@hotmail.com>
2021-06-07 14:27:35 +0200

    🎨 add meta for sharing

--- src/components/Meta.vue
+++ src/components/Meta.vue
@@ -2,15 +2,19 @@
   <teleport to='head'>
     <title>Mart Hesselink | Web Developer</title>
 
-    <meta name="description" content="Hello, I am Mart Hesselink. Web Developer & Music Lover. Need a site? Contact me!."/>
+    <meta name="description" content="Hello, I am Mart Hesselink. Web Developer & Music Lover. Need a website? Contact me!."/>
     <meta name="keywords" content="Mart Hesselink, Web Developer, Enschede, Website, Developer, WordPress"/>
     <meta name="author" content="Mart Hesselink"/>
 
     <meta name="robots" content="index"/>
     <meta name="robots" content= "nofollow"/>
 
-    <meta http-equiv="expires" content="86400"/>
+    <meta property="og:title" content="Mart Hesselink | Web Developer">
+    <meta property="og:description" content="Web Developer & Music Lover. Need a website? Contact me!">
+    <meta property="og:image" content="../assets/logo.png">
+    <meta property="og:url" content="https://marthesselink.nl/portfolio">
 
+    <meta http-equiv="expires" content="86400"/>
   </teleport>
 </template>
 
