a7c7dba8317ddb17b36bddf3e6bbd10e2c188b6e
Mart Hesselink <marthesselink@hotmail.com>
2021-06-07 14:30:36 +0200

    :art: more meta tags

--- src/components/Meta.vue
+++ src/components/Meta.vue
@@ -9,10 +9,12 @@
     <meta name="robots" content="index"/>
     <meta name="robots" content= "nofollow"/>
 
+    <meta property="og:type" content="website">
     <meta property="og:title" content="Mart Hesselink | Web Developer">
     <meta property="og:description" content="Web Developer & Music Lover. Need a website? Contact me!">
     <meta property="og:image" content="../assets/logo.png">
     <meta property="og:url" content="https://marthesselink.nl/portfolio">
+    <meta property="twitter:card" content="summary_large_image">
 
     <meta http-equiv="expires" content="86400"/>
   </teleport>
